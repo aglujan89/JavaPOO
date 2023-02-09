442de576768e8cac61dd14d1d222ab5d6b14270d
aglujan89 <adolfoglujan@gmail.com>
2023-02-09 20:42:05 -0300

    actualizo ejercicios

---
+++ RelacionesEjExtra3y4/src/Entities/Voto.java
@@ -0,0 +1,16 @@
+/*
+ */
+package Entities;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author fitog
+ */
+public class Voto {
+    private Alumno alumnoVotante;
+    private ArrayList<Alumno> alumnosVotados;
+    
+    
+}
